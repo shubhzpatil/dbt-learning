9eae66e381ab03482efd9d0e3f3ba35b901a22e6
Shubham <shubhamdpatil45@gmail.com>
2025-05-21 13:59:57 +0000

    added new column as full_name in stg_customers

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,6 +1,6 @@
 select
         id as customer_id,
         first_name,
-        last_name
-
+        last_name,
+        concat(first_name,last_name) as full_name
     from {{ source('jaffle_shop', 'customers') }}
